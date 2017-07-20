96777eee0368897bfe22f30d95fd70d0aec6fdc0
jakub.pawlowski <jakub.pawlowski@legimi.com>
2017-07-20 07:35:09 +0200

    update index.ts to match js

--- index.ts
+++ index.ts
@@ -1,87 +1,40 @@
-(function() {
-    var fs = require("fs");
-    var Handlebars = require("handlebars");
-    var moment = require("moment");
+const fs = require('fs');
+const path = require('path');
+const requireDir = require('require-dir');
+const Handlebars = require('handlebars');
 
-    function render(resume : any) {
-    	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
-    	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
-    	var expertLevel = ["expert","master"]
-    	return Handlebars.compile(tpl)({
-    		css: css,
-            resume: resume,
-            ext: {
-                skills: {
-                    expert:  (resume.skills as Array<any>).filter(v =>  v.level && expertLevel.indexOf(v.level.toLowerCase()) >= 0 ),
-                    other:   (resume.skills as Array<any>).filter(v =>  !v.level || expertLevel.indexOf(v.level.toLowerCase()) < 0 ),
-                }
-            }
-    	});
+const HBS_HELPERS = './theme/hbs-helpers';
+
+function render(resume : any) {
+  const css = fs.readFileSync(`${__dirname}/style.css`, 'utf-8');
+  const tpl = fs.readFileSync(`${__dirname}/resume.hbs`, 'utf-8');
+  const partialsDir = path.join(__dirname, 'theme/partials');
+  const filenames = fs.readdirSync(partialsDir);
+
+  filenames.forEach((filename : string) => {
+    const matches = /^([^.]+).hbs$/.exec(filename);
+    if (!matches) return;
+    const name = matches[1];
+    const filepath = path.join(partialsDir, filename);
+    const template = fs.readFileSync(filepath, 'utf8');
+
+    Handlebars.registerPartial(name, template);
+  });
+
+  var expertLevel = ["expert","master"];
+  return Handlebars.compile(tpl)({
+    css,
+    resume,
+    ext: {
+        skills: {
+            expert: (resume.skills as Array<any>).filter(function (v) { return v.level && expertLevel.indexOf(v.level.toLowerCase()) >= 0; }),
+            other: (resume.skills as Array<any>).filter(function (v) { return !v.level || expertLevel.indexOf(v.level.toLowerCase()) < 0; }),
+        }
     }
+  });
+}
 
-    module.exports = {
-    	render: render
-    };
+/* HANDLEBARS HELPERS */
+requireDir(HBS_HELPERS, { recurse: true });
 
-
-    /* HANDLEBARS HELPERS */
-    Handlebars.registerHelper('paragraphSplit', function(plaintext : any) {
-        var output = '';
-        var lines = plaintext instanceof Array ? plaintext.join('').split(/\r\n|\r|\n/g) : plaintext.split(/\r\n|\r|\n/g);
-        var i = 0;
-
-        while(i < lines.length) {
-            if(lines[i]) {
-                output += '<p>' + lines[i] + '</p>';
-            }
-            i += 1;
-        }
-
-        return new Handlebars.SafeString(output);
-    });
-
-    Handlebars.registerHelper('toLowerCase', function(str : string) {
-      return str.toLowerCase();
-    });
-
-    Handlebars.registerHelper('spaceToDash', function(str : string) {
-      return str.replace(/\s/g, '-').toLowerCase();
-    });
-
-    Handlebars.registerHelper('MY', function(date : any) {
-    	return moment(date.toString(), ['YYYY-MM-DD']).format('MMMM YYYY');
-    });
-
-    Handlebars.registerHelper('Y', function(date: any) {
-        return moment(date.toString(), ['YYYY-MM-DD']).format('YYYY');
-    });
-
-    Handlebars.registerHelper('DMY', function(date: any) {
-        return moment(date.toString(), ['YYYY-MM-DD']).format('D MMMM YYYY');
-    });
-
-    Handlebars.registerHelper('birthData', function(birth : any) {
-        var out = [];
-        if (birth && Object.keys(birth).length) {
-            out.push('<div> Born in ');
-            out.push(birth.place);
-
-            if (birth.place && birth.state) {
-                out.push(', ');
-            }
-
-            out.push(birth.state);
-
-            var year = birth.date &&
-                moment(birth.date.toString(), ['YYYY-MM-DD']).format('YYYY');
-
-            if (year && (birth.place || birth.state)) {
-                out.push(' in ');
-            }
-            out.push(year);
-            out.push('</div>');
-        }
-
-        return new Handlebars.SafeString(out.join(''));
-    });
-}());
+module.exports = { render };

8118daefb1276e36756cc502b109542635b94ec8
jakub.pawlowski <jakub.pawlowski@legimi.com>
2017-07-21 07:13:09 +0200

    better line split fo string arrays

--- theme/hbs-helpers/paragraph-split.js
+++ theme/hbs-helpers/paragraph-split.js
@@ -1,7 +1,7 @@
 const Handlebars = require('handlebars');
 
 Handlebars.registerHelper('paragraphSplit', (plaintext) => {
-  const lines = plaintext instanceof Array ? plaintext.join('').split(/\r\n|\r|\n/g) : plaintext.split(/\r\n|\r|\n/g);
+  const lines = plaintext instanceof Array ? plaintext.map(l => l.split(/\r\n|\r|\n/g)).reduce((flat, toFlatten) => flat.concat(toFlatten), []) : plaintext.split(/\r\n|\r|\n/g);
   const output = lines.filter(line => line).reduce((a, b) => `${a}<p>${b}</p>`, '');
   return new Handlebars.SafeString(output);
 });

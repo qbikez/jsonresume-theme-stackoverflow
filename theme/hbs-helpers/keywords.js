df990c778bb5e191a0711760ad3cb41122057812
jakub.pawlowski <pawlowskijakub@gmail.com>
2018-11-10 18:42:18 +0100

    add simple parsing of markdown-like directives in keywords

---
+++ theme/hbs-helpers/keywords.js
@@ -0,0 +1,13 @@
+const Handlebars = require('handlebars');
+
+Handlebars.registerHelper('keywordClass', (plaintext) => {
+    if (plaintext.startsWith("_")) return "secondary";
+    if (plaintext.startsWith("*") && plaintext.endsWith("*")) return "strong";
+  });
+  
+
+Handlebars.registerHelper('keywordMarkup', (plaintext) => {
+  if (plaintext.startsWith("_")) plaintext = plaintext.substring(1)
+  if (plaintext.startsWith("*") && plaintext.endsWith("*")) plaintext = plaintext.substring(1, plaintext.length-1)
+  return new Handlebars.SafeString(plaintext);
+});
